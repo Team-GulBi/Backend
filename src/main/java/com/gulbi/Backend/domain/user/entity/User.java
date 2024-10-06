e04bafeba432aed2b05302d40f9f696768fd6825
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-10-06 20:41:59 +0900

    chore: build 캐시 삭제

--- src/main/java/com/gulbi/Backend/domain/user/entity/User.java
+++ src/main/java/com/gulbi/Backend/domain/user/entity/User.java
@@ -1,13 +1,30 @@
 package com.gulbi.Backend.domain.user.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.gulbi.Backend.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
-public class User {
+@Table(name = "users")
+@Getter
+@NoArgsConstructor  // 기본 생성자
+@AllArgsConstructor  // 모든 필드를 사용하는 생성자
+@Builder  // 빌더 패턴을 위한 어노테이션
+public class User extends BaseEntity {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String nickname;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private String password;
 }

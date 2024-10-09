7890bf4a39b849853fdb3e532c76599ebda71222
woosungking <127114299+woosungking@users.noreply.github.com>
2024-10-09 21:48:13 +0900

    product 관련 클레스 파일 생성

---
+++ src/main/java/com/gulbi/Backend/domain/rental/product/service/ProductServiceImpl2.java
@@ -0,0 +1,9 @@
+package com.gulbi.Backend.domain.rental.product.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductServiceImpl2 implements ProductService2 {
+
+
+}

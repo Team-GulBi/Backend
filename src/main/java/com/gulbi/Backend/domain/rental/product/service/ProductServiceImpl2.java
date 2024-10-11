eae1c92caf6b85d4f02cd323fa77a5bcb0a6a4fa
woosungking <127114299+woosungking@users.noreply.github.com>
2024-10-12 03:37:27 +0900

    <Product> 원래 상품 등록 로직에서 review, image등 엔티티의 레포지토리를 그대로 사용했음, 단일책임의 원칙을 위반하는 느낌이라 간단한 findbyid같은 로직도 그냥 따로 서비스를 만들고 분리를 하였음

--- src/main/java/com/gulbi/Backend/domain/rental/product/service/ProductServiceImpl2.java
+++ src/main/java/com/gulbi/Backend/domain/rental/product/service/ProductServiceImpl2.java
@@ -8,6 +8,9 @@ import com.gulbi.Backend.domain.rental.product.entity.Product;
 import com.gulbi.Backend.domain.rental.product.repository.CategoryRepository;
 import com.gulbi.Backend.domain.rental.product.repository.ImageRepository;
 import com.gulbi.Backend.domain.rental.product.repository.ProductRepository2;
+import com.gulbi.Backend.domain.rental.product.service.category.CategoryService;
+import com.gulbi.Backend.domain.rental.review.dto.ReviewWithAvgProjection;
+import com.gulbi.Backend.domain.rental.review.service.ReviewService;
 import com.gulbi.Backend.domain.user.entity.User;
 import com.gulbi.Backend.domain.user.repository.UserRepository;
 import com.gulbi.Backend.global.util.Base64Util;
@@ -23,23 +26,24 @@ import java.util.Optional;
 @Service
 @RequiredArgsConstructor
 public class ProductServiceImpl2 implements ProductService2 {
-
+    private final UserRepository userRepository; // user관련 태호가 머지해주기 전에는 임시로 만들어서 쓸거임. 추후 머지 되면 없어짐.
     private final ProductRepository2 productRepository;
 
     private final ImageService imageService;
 
-    private final CategoryRepository categoryRepository;
+    private final ReviewService reviewService;
+    private final CategoryService categoryService;
 
-    private final ImageRepository imageRepository;
 
-    private final UserRepository userRepository; // user관련 태호가 머지해주기 전에는 임시로 만들어서 쓸거임. 추후 머지 되면 없어짐.
     @Override
     public void registerProduct(ProductRegisterRequest product, List<MultipartFile> images) {
 
-        String productName = product.getProductName();
-        String title = product.getTitle();
+
         User user = User.builder().email("1").phoneNumber("2").nickname("z").password("2").build();  // user관련 태호가 머지해주기 전에는 임시로 만들어서 쓸거임. 추후 머지 되면 없어짐.
         userRepository.save(user); // user관련 태호가 머지해주기 전에는 임시로 만들어서 쓸거임. 추후 머지 되면 없어짐.
+        try{
+        String productName = product.getProductName();
+        String title = product.getTitle();
         String bname = product.getBname();
         String sido = product.getSido();
         String sigungu = product.getSigungu();
@@ -51,16 +55,12 @@ public class ProductServiceImpl2 implements ProductService2 {
         Integer mcategoryId = Integer.parseInt(product.getMcategoryId());
         Integer bcategoryId = Integer.parseInt(product.getBcategoryId());
 
-        Optional<Category> scategory = categoryRepository.findById(scategoryId);
-        Optional<Category> mcategory = categoryRepository.findById(mcategoryId);
-        Optional<Category> bcategory = categoryRepository.findById(bcategoryId);
+        Category scategory = categoryService.getCategoryById(scategoryId);
+        Category mcategory = categoryService.getCategoryById(mcategoryId);
+        Category bcategory = categoryService.getCategoryById(bcategoryId);
 
-        if(!scategory.isPresent() || !mcategory.isPresent() || !bcategory.isPresent()){
-            System.out.println("해당 카테고리는 존재하지 않습니다.");
-            //예외처리 하기
-        }
 
-       try{
+
             String mainImg = Base64Util.MultipartFileToString(images.get(0));
 
 
@@ -76,9 +76,9 @@ public class ProductServiceImpl2 implements ProductService2 {
                 .title(title)
                 .views(0)
                 .rating(0)
-                .bCategory(bcategory.get())
-                .mCategory(mcategory.get())
-                .sCategory(scategory.get())
+                .bCategory(bcategory)
+                .mCategory(mcategory)
+                .sCategory(scategory)
                 .mainImage(mainImg)
                 .build();
 
@@ -96,8 +96,6 @@ public class ProductServiceImpl2 implements ProductService2 {
     @Override
     public ProductDetailResponse getProductDetail(Long productId) {
         Product product = productRepository.findById(productId).orElseThrow(()-> new RuntimeException());
-        List<Image> imageList = imageRepository.findByImageWithProduct(product.getId()).orElseThrow(()->new RuntimeException());
-
         final String tag = product.getTag();
         final String title = product.getTitle();
         final String productName = product.getName();
@@ -107,17 +105,21 @@ public class ProductServiceImpl2 implements ProductService2 {
         final String sigungu = product.getSigungu();
         final String bname = product.getBname();
         final String description = product.getDescription();
-        final String rationg = String.valueOf(product.getRating());
+        final String rating = String.valueOf(product.getRating());
         final String bCategory= product.getBCategory().getName();
         final String mcategory = product.getMCategory().getName();
         final String scategory = product.getSCategory().getName();
         final String created_at = String.valueOf(product.getCreatedAt());
 
-
+        List<Image> imageList = imageService.getImageByProductId(productId);
         final List<String> images = new ArrayList<>();
         for (Image image: imageList) {
             images.add(image.getUrl());
         }
+
+        List<ReviewWithAvgProjection>reviewWithAvg =reviewService.bringAllReview(productId);
+        System.out.println(reviewWithAvg.get(0).getContent());
+
 
         ProductDetailResponse response = ProductDetailResponse.builder()
                 .tag(tag)
@@ -129,12 +131,14 @@ public class ProductServiceImpl2 implements ProductService2 {
                 .sigungu(sigungu)
                 .bname(bname)
                 .description(description)
-                .rating(rationg)
+                .rating(rating)
                 .bcategory(bCategory)
                 .mcategory(mcategory)
                 .scategory(scategory)
                 .created_at(created_at)
-                .images(images).build();
+                .images(images)
+                .reviews(reviewWithAvg)
+                .build();
 
 
     return  response;

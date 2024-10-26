9025137ab979d3b9cfbe70635189f68c7280de34
woosungking <abuga991008@gmail.com>
2024-10-26 02:31:06 -0700

    <CreateDummyData>
    getRandomCategory - 카테고리 연관관계 고려, 대,중,소 분류 카테고리 반환

--- src/main/java/com/gulbi/Backend/domain/rental/product/util/CreateDummyData.java
+++ src/main/java/com/gulbi/Backend/domain/rental/product/util/CreateDummyData.java
@@ -1,15 +1,21 @@
 package com.gulbi.Backend.domain.rental.product.util;
 
+import com.gulbi.Backend.domain.rental.product.dto.CategoryProjection;
+import com.gulbi.Backend.domain.rental.product.entity.Category;
+import com.gulbi.Backend.domain.rental.product.repository.CategoryRepository;
 import com.gulbi.Backend.domain.user.entity.User;
 import com.gulbi.Backend.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.*;
-
+@RequiredArgsConstructor
+@Component
 public class CreateDummyData {
 
-    public static String[] getRandomRegion(){
+    private final CategoryRepository categoryRepository;
+
+    public String[] getRandomRegion(){
         final Map<String,List<String>> sigunguMap = new HashMap<>();
         sigunguMap.put("서울",Arrays.asList("강남구","송파구","성북구","강서구","금천구","노원구","도봉구","동작구","마포구","강동구","양천구"));
         sigunguMap.put("인천",Arrays.asList("부평구","계양구","서구","중구","미추홀구","연수구","강화군","옹진군"));
@@ -31,12 +37,30 @@ public class CreateDummyData {
         return region;
     }
 
-    public static String getRandomProductName(){
+    public  String getRandomProductName(){
         final String[] productName={"삼성","에플","LG"};
         final String[] productName2={"노트북","휴대폰","냉장고","세탁기","TV","이어폰","스피커","PC"};
         String randomProductName = productName[(int)(Math.random()*productName.length)];
         String randomProductName2 = productName2[(int)(Math.random()*productName2.length)];
         return randomProductName +randomProductName2;
+    }
+
+    public List<Category> getRandomCategory(){
+        List<CategoryProjection> bcategoryList = categoryRepository.findAllNoParent();
+        int randomIndex = (int)(Math.random()*bcategoryList.size());
+        Category bcategory = (Category) bcategoryList.get(randomIndex);
+
+        List<Category> mcategoryList = categoryRepository.findByParent(bcategory);
+        randomIndex = (int)(Math.random()*mcategoryList.size());
+        Category mcategory = mcategoryList.get(randomIndex);
+
+        List<Category> scategoryList = categoryRepository.findByParent(mcategory);
+        randomIndex = (int)(Math.random()*scategoryList.size());
+        Category scategory = scategoryList.get(randomIndex);
+
+        List<Category> randomCategories = Arrays.asList(bcategory,mcategory,scategory);
+        return randomCategories;
+
     }
 
 

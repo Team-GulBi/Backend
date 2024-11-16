3eb7f023089e54a9156997dc5dd01727bf3364a6
woosungking <abuga991008@gmail.com>
2024-11-17 00:26:42 +0900

    feat<.findProductDtoById>

--- src/main/java/com/gulbi/Backend/domain/rental/product/repository/ProductRepository.java
+++ src/main/java/com/gulbi/Backend/domain/rental/product/repository/ProductRepository.java
@@ -1,6 +1,7 @@
 package com.gulbi.Backend.domain.rental.product.repository;
 
-import com.gulbi.Backend.domain.rental.product.dto.ProductResponseDto;
+import com.gulbi.Backend.domain.rental.product.dto.product.ProductDto;
+import com.gulbi.Backend.domain.rental.product.dto.product.ProductOverViewResponse;
 import com.gulbi.Backend.domain.rental.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,17 +12,14 @@ import java.util.List;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product,Long> {
-//    @Query("SELECT p.id AS id, p.mainImage AS mainImage, p.title AS title, p.price AS price FROM Product p WHERE p.title LIKE CONCAT('%', :query, '%')")
-//@Query(value = "SELECT p.id AS id, p.main_image AS mainImage, p.title AS title, p.price AS price FROM products p WHERE p.title LIKE CONCAT('%', :query, '%')", nativeQuery = true)
-//public List<ProductResponseProjection> findProductByQuery(@Param("query")String query);
 
+    @Query(value = "SELECT p.id AS id, p.main_image AS mainImage, p.title AS title, p.price AS price FROM products p WHERE p.title LIKE CONCAT('%', :query, '%')", nativeQuery = true)
+    public List<ProductOverViewResponse> findProductByQuery(@Param("query")String query);
 
+    @Query("SELECT new com.gulbi.Backend.domain.rental.product.dto.product.ProductDto(p.id, p.tag, p.title, p.name, p.views, p.price, p.sido, p.sigungu, p.bname, p.description, p.rating, p.mainImage) " +
+            "FROM Product p WHERE p.id = :id")
+    ProductDto findProductDtoById(@Param("id") Long id);
 
-
-
-//
-    @Query("SELECT new com.gulbi.Backend.domain.rental.product.dto.ProductResponseDto(p.id , p.mainImage, p.title, p.price) FROM Product p WHERE p.title LIKE CONCAT('%', :query, '%')")
-    public List<ProductResponseDto> findProductByQuery(@Param("query") String query);
 
 
 }

c9f857ebfbf8e8e41173b5eacb886ae36bdcaf5d
woosungking <abuga991008@gmail.com>
2024-11-17 01:52:12 +0900

    refactor<ProductCrudService> 상품 CRUD 서비스
    확장 가능성 고려, CRUD와 성격이 유사한 기능을 담당하는 서비스임

---
+++ src/main/java/com/gulbi/Backend/domain/rental/product/service/ProductCrudService.java
@@ -0,0 +1,13 @@
+package com.gulbi.Backend.domain.rental.product.service;
+
+import com.gulbi.Backend.domain.rental.product.dto.product.ProductDto;
+import com.gulbi.Backend.domain.rental.product.dto.product.ProductOverViewResponse;
+import com.gulbi.Backend.domain.rental.product.entity.Product;
+
+import java.util.List;
+
+public interface ProductCrudService {
+    public void saveProduct(Product product);
+    public ProductDto findProductById(Long productId);
+    public List<ProductOverViewResponse> findProductOverViewByQuery(String query);
+}
---
+++ src/main/java/com/gulbi/Backend/domain/rental/product/service/ProductCrudServiceImpl.java
@@ -0,0 +1,30 @@
+package com.gulbi.Backend.domain.rental.product.service;
+
+import com.gulbi.Backend.domain.rental.product.dto.product.ProductDto;
+import com.gulbi.Backend.domain.rental.product.dto.product.ProductOverViewResponse;
+import com.gulbi.Backend.domain.rental.product.entity.Product;
+import com.gulbi.Backend.domain.rental.product.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+@RequiredArgsConstructor
+public class ProductCrudServiceImpl implements ProductCrudService{
+    private final ProductRepository productRepository;
+    @Override
+    public void saveProduct(Product product) {
+        productRepository.save(product);
+    }
+
+    @Override
+    public ProductDto findProductById(Long productId) {
+        return productRepository.findProductDtoById(productId)
+                .orElseThrow(() -> new RuntimeException("Product not found: " + productId));
+    }
+
+    @Override
+    public List<ProductOverViewResponse> findProductOverViewByQuery(String query) {
+        return productRepository.findProductByQuery(query);
+    }
+}

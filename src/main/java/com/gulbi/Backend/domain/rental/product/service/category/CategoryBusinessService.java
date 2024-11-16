657aa2b6c56d379f0d817aaa69facc02476b353f
woosungking <abuga991008@gmail.com>
2024-11-17 01:36:40 +0900

    style<> 의미전달을 위한 이름 변경
    CategoryDto => CategoryInProductRegisterDto

--- src/main/java/com/gulbi/Backend/domain/rental/product/service/category/CategoryBusinessService.java
+++ src/main/java/com/gulbi/Backend/domain/rental/product/service/category/CategoryBusinessService.java
@@ -1,8 +1,8 @@
 package com.gulbi.Backend.domain.rental.product.service.category;
 
-import com.gulbi.Backend.domain.rental.product.dto.CategoryDto;
-import com.gulbi.Backend.domain.rental.product.dto.ProductRegisterRequestDto;
+import com.gulbi.Backend.domain.rental.product.dto.category.CategoryInProductRegisterDto;
+import com.gulbi.Backend.domain.rental.product.dto.request.ProductRegisterRequestDto;
 
 public interface CategoryBusinessService {
-    public CategoryDto resolveCategories(ProductRegisterRequestDto productRegisterRequestDto);
+    public CategoryInProductRegisterDto resolveCategories(ProductRegisterRequestDto productRegisterRequestDto);
 }
--- src/main/java/com/gulbi/Backend/domain/rental/product/service/category/CategoryBusinessServiceImpl.java
+++ src/main/java/com/gulbi/Backend/domain/rental/product/service/category/CategoryBusinessServiceImpl.java
@@ -1,19 +1,20 @@
 package com.gulbi.Backend.domain.rental.product.service.category;
 
-import com.gulbi.Backend.domain.rental.product.dto.CategoryDto;
-import com.gulbi.Backend.domain.rental.product.dto.ProductRegisterRequestDto;
+import com.gulbi.Backend.domain.rental.product.dto.category.CategoryInProductRegisterDto;
+import com.gulbi.Backend.domain.rental.product.dto.request.ProductRegisterRequestDto;
 import com.gulbi.Backend.domain.rental.product.entity.Category;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
+@Service
 @RequiredArgsConstructor
 public class CategoryBusinessServiceImpl implements CategoryBusinessService{
     private final CategoryCrudService categoryCrudService;
     @Override
-    public CategoryDto resolveCategories(ProductRegisterRequestDto productRegisterRequestDto) {
+    public CategoryInProductRegisterDto resolveCategories(ProductRegisterRequestDto productRegisterRequestDto) {
         Category bCategory = categoryCrudService.getCategoryById(Integer.valueOf(productRegisterRequestDto.getBcategoryId()));
         Category mCategory = categoryCrudService.getCategoryById(Integer.valueOf(productRegisterRequestDto.getMcategoryId()));
         Category sCategory = categoryCrudService.getCategoryById(Integer.valueOf(productRegisterRequestDto.getScategoryId()));
-        return CategoryDto.of(bCategory, mCategory, sCategory);
+        return CategoryInProductRegisterDto.of(bCategory, mCategory, sCategory);
     }
 }

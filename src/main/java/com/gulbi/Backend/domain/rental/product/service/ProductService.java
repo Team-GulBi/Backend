08dcd72e21d75c339ede13ed5a5fc44a4a9d1f8e
woosungking <abuga991008@gmail.com>
2024-11-16 23:04:34 +0900

    refactor<.registerProduct> 메서드 간소화
    팩토리 메서드, Dto 사용, 기존 로직들을 분리하여 다른 메서드로 책임을 전가함

--- src/main/java/com/gulbi/Backend/domain/rental/product/service/ProductService.java
+++ src/main/java/com/gulbi/Backend/domain/rental/product/service/ProductService.java
@@ -1,19 +1,19 @@
 package com.gulbi.Backend.domain.rental.product.service;
 
-import com.gulbi.Backend.domain.rental.product.dto.ProductDetailResponse;
-import com.gulbi.Backend.domain.rental.product.dto.ProductRegisterRequest;
-import com.gulbi.Backend.domain.rental.product.dto.ProductResponseDto;
+import com.gulbi.Backend.domain.rental.product.dto.product.ProductResponseProjection;
+import com.gulbi.Backend.domain.rental.product.dto.response.ProductDetailResponseDto;
+import com.gulbi.Backend.domain.rental.product.dto.request.ProductRegisterRequestDto;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
 
 public interface ProductService {
-    public void registerProduct(ProductRegisterRequest product, List<MultipartFile> images) throws IOException;
-    public ProductDetailResponse getProductDetail(Long productId);
+    public void registerProduct(ProductRegisterRequestDto product, List<MultipartFile> images) throws IOException;
+    public ProductDetailResponseDto getProductDetail(Long productId);
 
 //    public List<ProductResponseProjection> searchProductWithTitle(String query);
-public List<ProductResponseDto> searchProductWithTitle(String query);
+public List<ProductResponseProjection> searchProductWithTitle(String query);
 
 
 

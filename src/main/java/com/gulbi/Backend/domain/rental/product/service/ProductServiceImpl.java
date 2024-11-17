c0fafd8e2552e26f6bcd4ca1a1349789724e3774
woosungking <abuga991008@gmail.com>
2024-11-17 21:08:15 +0900

    refactor<.registerProduct>
    .processProductRegisterData
    .resolveUser
    .createProduct
    .saveProductAndImages
    기존 상품 저장 로직을 기능에 따라 분리

--- src/main/java/com/gulbi/Backend/domain/rental/product/service/ProductServiceImpl.java
+++ src/main/java/com/gulbi/Backend/domain/rental/product/service/ProductServiceImpl.java
@@ -1,16 +1,13 @@
 package com.gulbi.Backend.domain.rental.product.service;
 
-import com.gulbi.Backend.domain.rental.product.dto.ProductImageDto;
-import com.gulbi.Backend.domain.rental.product.dto.category.CategoryInProductRegisterDto;
+import com.gulbi.Backend.domain.rental.product.dto.category.CategoryInProductDto;
 import com.gulbi.Backend.domain.rental.product.dto.product.ProductDto;
 import com.gulbi.Backend.domain.rental.product.dto.product.ProductRegisterDto;
 import com.gulbi.Backend.domain.rental.product.dto.product.ProductOverViewResponse;
 import com.gulbi.Backend.domain.rental.product.dto.request.ProductRegisterRequestDto;
 import com.gulbi.Backend.domain.rental.product.dto.response.ProductDetailResponseDto;
-import com.gulbi.Backend.domain.rental.product.entity.Image;
 import com.gulbi.Backend.domain.rental.product.entity.Product;
 import com.gulbi.Backend.domain.rental.product.factory.ProductFactory;
-import com.gulbi.Backend.domain.rental.product.repository.ProductRepository;
 import com.gulbi.Backend.domain.rental.product.service.category.CategoryBusinessService;
 import com.gulbi.Backend.domain.rental.product.vo.MainImage;
 import com.gulbi.Backend.domain.rental.product.vo.ProductImageDtoList;
@@ -21,35 +18,24 @@ import com.gulbi.Backend.domain.user.repository.UserRepository;
 import com.gulbi.Backend.global.util.Base64Util;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ProductServiceImpl implements ProductService {
-    private final UserRepository userRepository; // user관련 태호가 머지해주기 전에는 임시로 만들어서 쓸거임. 추후 머지 되면 없어짐.
+    private final UserRepository userRepository;
     private final ProductCrudService productCrudService;
     private final ImageService imageService;
     private final ReviewService reviewService;
     private final CategoryBusinessService categoryBusinessService;
 
-
+    // Public methods
     @Override
-    public void registerProduct(ProductRegisterRequestDto product, List<MultipartFile> images) throws IOException {
-
-        User user = User.builder().email("1").phoneNumber("2").nickname("z").password("2").build();  // user관련 태호가 머지해주기 전에는 임시로 만들어서 쓸거임. 추후 머지 되면 없어짐.
-        userRepository.save(user); // user관련 태호가 머지해주기 전에는 임시로 만들어서 쓸거임. 추후 머지 되면 없어짐.
-
-        CategoryInProductRegisterDto categoryInProductRegisterDto = categoryBusinessService.resolveCategories(product);
-        ProductRegisterDto productRegisterDto = product.createProductDto();
-        MainImage mainImg = MainImage.of(Base64Util.MultipartFileToString(images.get(0)));
-        Product product1 = ProductFactory.createProduct(productRegisterDto, categoryInProductRegisterDto, user , mainImg);
-        productCrudService.saveProduct(product1);
-        imageService.registerImageWithProduct(images, product1);
-
+    public void registerProduct(ProductRegisterRequestDto product) throws IOException {
+        Product product1 = createProduct(product);
+        saveProductAndImages(product1, product);
     }
 
     @Override
@@ -57,7 +43,7 @@ public class ProductServiceImpl implements ProductService {
         ProductDto product = productCrudService.findProductById(productId);
         ProductImageDtoList imageList = imageService.getImageByProductId(productId);
         List<ReviewWithAvgProjection> reviewWithAvg = reviewService.bringAllReview(productId);
-        return ProductDetailResponseDto.of(product,imageList,reviewWithAvg);
+        return ProductDetailResponseDto.of(product, imageList, reviewWithAvg);
     }
 
     @Override
@@ -65,5 +51,42 @@ public class ProductServiceImpl implements ProductService {
         return productCrudService.findProductOverViewByQuery(query);
     }
 
+    @Override
+    public Product processProductRegisterData(ProductRegisterRequestDto product) {
+        User user = resolveUser();
+        CategoryInProductDto categoryInProduct = categoryBusinessService.resolveCategories(product);
+        ProductRegisterDto productRegisterDto = ProductRegisterDto.of(product, categoryInProduct, user);
+        return ProductFactory.createProduct(productRegisterDto);
+    }
 
+    @Override
+    public User resolveUser() {
+        User user = User.builder()
+                .email("1")
+                .phoneNumber("2")
+                .nickname("z")
+                .password("2")
+                .build();
+        userRepository.save(user);
+        return user;
+    }
+
+    @Override
+    public ProductRegisterDto createProductRegisterDto() {
+        return null;
+    }
+
+    @Override
+    public Product createProduct(ProductRegisterRequestDto product) {
+        User user = resolveUser(); // 유저 조회
+        CategoryInProductDto categoryInProduct = categoryBusinessService.resolveCategories(product); //카테고리 조회
+        ProductRegisterDto productRegisterDto = ProductRegisterDto.of(product, categoryInProduct, user);
+        return ProductFactory.createProduct(productRegisterDto);
+    }
+
+    @Override
+    public void saveProductAndImages(Product product, ProductRegisterRequestDto productRequest){
+        productCrudService.saveProduct(product);
+        imageService.registerImageWithProduct(productRequest, product);
+    }
 }

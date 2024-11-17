17cac12cc7af6442ac5728da986a171e02ad78f0
woosungking <abuga991008@gmail.com>
2024-11-17 21:01:19 +0900

    refactor<> 클래스명 변경

--- src/main/java/com/gulbi/Backend/domain/rental/product/dto/category/CategoryInProductDto.java
+++ src/main/java/com/gulbi/Backend/domain/rental/product/dto/category/CategoryInProductDto.java
@@ -4,18 +4,18 @@ import com.gulbi.Backend.domain.rental.product.entity.Category;
 import lombok.Getter;
 
 @Getter
-public class CategoryInProductRegisterDto {
+public class CategoryInProductDto {
     private final Category bCategoryId;
     private final Category mCategoryId;
     private final Category sCategoryId;
 
-    private CategoryInProductRegisterDto(Category bCategoryId, Category mCategoryId, Category sCategoryId){
+    private CategoryInProductDto(Category bCategoryId, Category mCategoryId, Category sCategoryId){
         this.bCategoryId = bCategoryId;
         this.mCategoryId = mCategoryId;
         this.sCategoryId = sCategoryId;
     }
 
-    public static CategoryInProductRegisterDto of(Category bCategoryId, Category mCategoryId, Category sCategoryId){
-        return new CategoryInProductRegisterDto(bCategoryId, mCategoryId, sCategoryId);
+    public static CategoryInProductDto of(Category bCategoryId, Category mCategoryId, Category sCategoryId){
+        return new CategoryInProductDto(bCategoryId, mCategoryId, sCategoryId);
     }
 }

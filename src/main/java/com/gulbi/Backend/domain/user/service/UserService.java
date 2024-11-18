96344de245aa6b051a722ad544a6471d819acd79
woosungking <abuga991008@gmail.com>
2024-11-18 19:36:03 +0900

    feat<.getDummyUser> 더미 유저 객체를 가지고 오는 메서드

---
+++ src/main/java/com/gulbi/Backend/domain/user/service/UserService.java
@@ -0,0 +1,68 @@
+package com.gulbi.Backend.domain.user.service;
+
+import com.gulbi.Backend.domain.user.dto.LoginRequestDto;
+import com.gulbi.Backend.domain.user.dto.RegisterRequestDto;
+import com.gulbi.Backend.global.util.JwtUtil;
+import com.gulbi.Backend.domain.user.entity.User;
+import com.gulbi.Backend.domain.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
+    private final AuthenticationManager authenticationManager;
+
+    public void register(RegisterRequestDto request){
+        User user = User.builder()
+                .nickname(request.getNickname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .phoneNumber(request.getPhoneNumber())
+                .build();
+        userRepository.save(user);
+    }
+
+    public String login(LoginRequestDto request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(request.getEmail(),request.getPassword())
+        );
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+        return jwtUtil.generateToken(user.getEmail(), user.getId()  );
+    }
+
+    public User getAuthenticatedUser() {
+        String email = getAuthenticatedEmail();
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new RuntimeException("Authenticated User not found"));
+    }
+    public User getDummyUser(){
+        User user = User.builder()
+                .email("1")
+                .phoneNumber("2")
+                .nickname("z")
+                .password("2")
+                .build();
+        userRepository.save(user);
+        return user;
+    }
+    private String getAuthenticatedEmail() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        if (principal instanceof UserDetails) {
+            return ((UserDetails) principal).getUsername();
+        } else {
+            throw new RuntimeException("No authenticated user");
+        }
+    }
+}

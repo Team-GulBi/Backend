7b3d27777e9f94d36019eddf1b7708137d725935
woosungking <abuga991008@gmail.com>
2024-11-19 15:55:55 +0900

    refactor<.registerImageWithProduct> 기존 based64 => aws s3 이용 url반환 하도록 변경

--- src/main/java/com/gulbi/Backend/domain/rental/product/service/ImageServiceImpl.java
+++ src/main/java/com/gulbi/Backend/domain/rental/product/service/ImageServiceImpl.java
@@ -1,16 +1,21 @@
-package com.gulbi.Backend.domain.rental.product.service;
+package com.gulbi.Backend.domain.rental.product.service.image;
 
+import com.gulbi.Backend.domain.rental.product.dto.ProductImageDto;
+import com.gulbi.Backend.domain.rental.product.dto.request.ProductRegisterRequestDto;
 import com.gulbi.Backend.domain.rental.product.entity.Image;
 import com.gulbi.Backend.domain.rental.product.entity.Product;
+import com.gulbi.Backend.domain.rental.product.factory.ImageFactory;
 import com.gulbi.Backend.domain.rental.product.repository.ImageRepository;
+import com.gulbi.Backend.domain.rental.product.vo.ProductImageDtoList;
+import com.gulbi.Backend.domain.rental.product.vo.ProductImages;
 import com.gulbi.Backend.global.util.Base64Util;
+import com.gulbi.Backend.global.util.FileSender;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Base64;
 import java.util.List;
 
 @Service
@@ -18,25 +23,29 @@ import java.util.List;
 public class ImageServiceImpl implements ImageService {
 
     private final ImageRepository imageRepository;
+    private final FileSender fileSender;
 
     @Override
-    public void registerImageWithProduct(List<MultipartFile> files, Product product) {
-        List<Image> imageList = new ArrayList<>();
-
-        for (MultipartFile file : files) {
+    public void registerImageWithProduct(ProductRegisterRequestDto files, Product product) {
+        ProductImages productImages = files.getProductImages();
+        for (MultipartFile file : productImages.getProductImages() ) {
             try {
-                String strBytes = Base64Util.MultipartFileToString(file); // global > util에 만들어놨어용
-                Image image = Image.builder().url(strBytes).product(product).build();
-                imageList.add(image);
+                String imageUrl = fileSender.sendFile(file);
+                Image image = ImageFactory.createImage(imageUrl, product);
+                imageRepository.save(image);
             } catch (IOException e) {
                 throw new RuntimeException("Error processing file: " + file.getOriginalFilename(), e);
             }
         }
 
-        // 이미지 리스트를 한 번에 저장
-        if (!imageList.isEmpty()) {
-            imageRepository.saveAll(imageList);
-        }
+    }
+
+
+    @Override
+    public ProductImageDtoList getImageByProductId(Long productId) {
+        List<ProductImageDto> images = imageRepository.findByImageWithProduct(productId);
+        return ProductImageDtoList.of(images);
+
     }
 
 

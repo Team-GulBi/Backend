12e89f549febb79e123b4d91a7d3afffa50c49dd
woosungking <abuga991008@gmail.com>
2024-11-19 19:28:33 +0900

    feat<.uploadImagesToS3> s3이미지 업로드 로직 분리

--- src/main/java/com/gulbi/Backend/domain/rental/product/service/image/ImageService.java
+++ src/main/java/com/gulbi/Backend/domain/rental/product/service/image/ImageService.java
@@ -1,12 +1,14 @@
 package com.gulbi.Backend.domain.rental.product.service.image;
 
-import com.gulbi.Backend.domain.rental.product.dto.request.ProductRegisterRequestDto;
 import com.gulbi.Backend.domain.rental.product.entity.Product;
-import com.gulbi.Backend.domain.rental.product.vo.ProductImageDtoList;
-import com.gulbi.Backend.domain.rental.product.vo.ProductImages;
+import com.gulbi.Backend.domain.rental.product.vo.image.ImageUrls;
+import com.gulbi.Backend.domain.rental.product.dto.ProductImageDtos;
+import com.gulbi.Backend.domain.rental.product.vo.image.Images;
+import com.gulbi.Backend.domain.rental.product.vo.image.ProductImages;
 
 public interface ImageService {
-    public void registerImageWithProduct(ProductRegisterRequestDto files, Product product);
-    public ProductImageDtoList getImageByProductId(Long productId);
-
+    public void registerImageWithProduct(ProductImages files, Product product);
+    public ImageUrls uploadImagesToS3(ProductImages productImages);
+    public ProductImageDtos getImageByProductId(Long productId);
+    public void saveImages(Images images);
 }
--- src/main/java/com/gulbi/Backend/domain/rental/product/service/image/ImageServiceImpl.java
+++ src/main/java/com/gulbi/Backend/domain/rental/product/service/image/ImageServiceImpl.java
@@ -1,20 +1,20 @@
 package com.gulbi.Backend.domain.rental.product.service.image;
 
 import com.gulbi.Backend.domain.rental.product.dto.ProductImageDto;
-import com.gulbi.Backend.domain.rental.product.dto.request.ProductRegisterRequestDto;
 import com.gulbi.Backend.domain.rental.product.entity.Image;
 import com.gulbi.Backend.domain.rental.product.entity.Product;
 import com.gulbi.Backend.domain.rental.product.factory.ImageFactory;
 import com.gulbi.Backend.domain.rental.product.repository.ImageRepository;
-import com.gulbi.Backend.domain.rental.product.vo.ProductImageDtoList;
-import com.gulbi.Backend.domain.rental.product.vo.ProductImages;
-import com.gulbi.Backend.global.util.Base64Util;
+import com.gulbi.Backend.domain.rental.product.vo.image.ImageUrl;
+import com.gulbi.Backend.domain.rental.product.vo.image.ImageUrls;
+import com.gulbi.Backend.domain.rental.product.dto.ProductImageDtos;
+import com.gulbi.Backend.domain.rental.product.vo.image.Images;
+import com.gulbi.Backend.domain.rental.product.vo.image.ProductImages;
 import com.gulbi.Backend.global.util.FileSender;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,26 +26,36 @@ public class ImageServiceImpl implements ImageService {
     private final FileSender fileSender;
 
     @Override
-    public void registerImageWithProduct(ProductRegisterRequestDto files, Product product) {
-        ProductImages productImages = files.getProductImages();
-        for (MultipartFile file : productImages.getProductImages() ) {
-            try {
-                String imageUrl = fileSender.sendFile(file);
-                Image image = ImageFactory.createImage(imageUrl, product);
-                imageRepository.save(image);
-            } catch (IOException e) {
-                throw new RuntimeException("Error processing file: " + file.getOriginalFilename(), e);
+    public void registerImageWithProduct(ProductImages productImages, Product product) {
+        ImageUrls imageUrls = uploadImagesToS3(productImages);
+        Images images = ImageFactory.createImagesFromUrls(imageUrls, product);
+        saveImages(images);
+    }
+
+    @Override
+    public ImageUrls uploadImagesToS3(ProductImages productImages) {
+        try{
+            List<ImageUrl> imageUrlList = new ArrayList<>();
+            for (MultipartFile file : productImages.getProductImages()){
+                ImageUrl imageUrl = ImageUrl.of(fileSender.sendFile(file));
+                imageUrlList.add(imageUrl);
             }
+            return ImageUrls.of(imageUrlList);
+        }catch (Exception e){
+            throw new RuntimeException();
         }
+    }
+
+    @Override
+    public ProductImageDtos getImageByProductId(Long productId) {
+        List<ProductImageDto> images = imageRepository.findByImageWithProduct(productId);
+        return ProductImageDtos.of(images);
 
     }
 
-
     @Override
-    public ProductImageDtoList getImageByProductId(Long productId) {
-        List<ProductImageDto> images = imageRepository.findByImageWithProduct(productId);
-        return ProductImageDtoList.of(images);
-
+    public void saveImages(Images images){
+        imageRepository.saveAll(images.getImages());
     }
 
 

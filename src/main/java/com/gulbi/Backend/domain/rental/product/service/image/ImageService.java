e2ddd0327ffe7b7b4d6d5fde9a9deda1f443142a
woosungking <abuga991008@gmail.com>
2024-11-19 15:55:55 +0900

    refactor<.registerImageWithProduct> 기존 based64 => aws s3 이용 url반환 하도록 변경

--- src/main/java/com/gulbi/Backend/domain/rental/product/service/image/ImageService.java
+++ src/main/java/com/gulbi/Backend/domain/rental/product/service/image/ImageService.java
@@ -3,6 +3,7 @@ package com.gulbi.Backend.domain.rental.product.service.image;
 import com.gulbi.Backend.domain.rental.product.dto.request.ProductRegisterRequestDto;
 import com.gulbi.Backend.domain.rental.product.entity.Product;
 import com.gulbi.Backend.domain.rental.product.vo.ProductImageDtoList;
+import com.gulbi.Backend.domain.rental.product.vo.ProductImages;
 
 public interface ImageService {
     public void registerImageWithProduct(ProductRegisterRequestDto files, Product product);

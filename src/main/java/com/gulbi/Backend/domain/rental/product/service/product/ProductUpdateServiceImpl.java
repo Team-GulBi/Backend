3a29df7b2247852f6cfef799ddae7b38c37066b9
woosungking <abuga991008@gmail.com>
2024-11-23 21:02:32 +0900

    feat<ProductUpdatingService> 상품 업데이트 서비스

---
+++ src/main/java/com/gulbi/Backend/domain/rental/product/service/product/ProductUpdateServiceImpl.java
@@ -0,0 +1,50 @@
+package com.gulbi.Backend.domain.rental.product.service.product;
+
+import com.gulbi.Backend.domain.rental.product.dto.product.request.ProductCategoryUpdateRequestDto;
+import com.gulbi.Backend.domain.rental.product.dto.product.request.ProductImageCreateRequestDto;
+import com.gulbi.Backend.domain.rental.product.dto.product.request.ProductImageDeleteRequestDto;
+import com.gulbi.Backend.domain.rental.product.dto.product.request.ProductUpdateRequestDto;
+import com.gulbi.Backend.domain.rental.product.entity.Product;
+import com.gulbi.Backend.domain.rental.product.service.category.CategoryBusinessService;
+import com.gulbi.Backend.domain.rental.product.service.image.ImageService;
+import com.gulbi.Backend.domain.rental.product.vo.image.ProductImageCollection;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ProductUpdateServiceImpl implements ProductUpdatingService{
+    private final ProductCrudService productCrudService;
+    private final CategoryBusinessService categoryBusinessService;
+    private final ImageService imageService;
+    @Override
+    public void updateProductViews(Long productId){
+        productCrudService.updateProductViews(productId);
+    }
+
+    @Override
+    public void updateProductInfo(ProductUpdateRequestDto productUpdateRequestDto, ProductCategoryUpdateRequestDto productCategoryUpdateRequestDto, ProductImageDeleteRequestDto productImageDeleteRequestDto, ProductImageCreateRequestDto productImageCreateRequestDto){
+        Optional.ofNullable(productCategoryUpdateRequestDto)
+                .map(categoryBusinessService::resolveCategories)
+                .ifPresent(productUpdateRequestDto::setCategoryInProduct);
+        Optional.of(productUpdateRequestDto)
+                .ifPresent(productCrudService::updateProductInfo);
+//
+//        Optional.ofNullable(productImageDeleteRequestDto)
+//                        .ifPresent(imageService::deleteImages);
+//
+//        Optional.ofNullable(productImageCreateRequestDto)
+//                .ifPresent(dto ->{
+//                        productImageCreateRequestDto.setProductId(productUpdateRequestDto.getProductId());
+//                        imageService.registerImageWithProduct(imageService.uploadImagesToS3(dto.getProductImageCollection()), resolveProduct(dto.getProductId()));
+//                });
+
+    }
+
+    private Product resolveProduct(Long productId){
+        return productCrudService.getProductById(productId);
+    }
+
+}

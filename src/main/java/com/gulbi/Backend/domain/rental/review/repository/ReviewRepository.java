51f8b9a6d6d2a501eb3be2c416cb20d36c56ba41
woosungking <abuga991008@gmail.com>
2024-11-25 16:04:04 +0900

    feat<> 리뷰 삭제 및 업데이트

--- src/main/java/com/gulbi/Backend/domain/rental/review/repository/ReviewRepository.java
+++ src/main/java/com/gulbi/Backend/domain/rental/review/repository/ReviewRepository.java
@@ -2,7 +2,9 @@ package com.gulbi.Backend.domain.rental.review.repository;
 
 import com.gulbi.Backend.domain.rental.review.dto.ReviewWithAvgProjection;
 import com.gulbi.Backend.domain.rental.review.entity.Review;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -17,6 +19,17 @@ public interface ReviewRepository extends JpaRepository<Review,Long> {
     @Query("SELECT r.id AS id, r.rating AS rating, r.content AS content, AVG(r.rating) AS averageRating " +
         "FROM Review r WHERE r.product.id = :productId GROUP BY r.id")
     public List<ReviewWithAvgProjection> findAllReviewAndAvgByProductId(@Param("productId") Long productId);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Review r WHERE r.id = :reviewId")
+    public void deleteReviewByReviewId(@Param("reviewId") Long reviewId);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Review r SET r.rating = :rating, r.content = :content WHERE r.id = :reviewId")
+    public void updateReviewInfo(@Param("rating") Integer rating, @Param("content")String content, @Param("reviewId") Long reviewId);
+
 
 
 }

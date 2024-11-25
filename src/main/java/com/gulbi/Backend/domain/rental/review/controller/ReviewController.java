b2a07b27362344b29cc960e6c5a6a2e00c4f13b2
woosungking <abuga991008@gmail.com>
2024-11-26 00:48:56 +0900

    feat<.createReiew> dto 유효성 검사

--- src/main/java/com/gulbi/Backend/domain/rental/review/controller/ReviewController.java
+++ src/main/java/com/gulbi/Backend/domain/rental/review/controller/ReviewController.java
@@ -5,9 +5,11 @@ import com.gulbi.Backend.domain.rental.review.dto.ReviewUpdateRequestDto;
 import com.gulbi.Backend.domain.rental.review.service.ReviewService;
 import com.gulbi.Backend.domain.user.response.SuccessCode;
 import com.gulbi.Backend.global.response.RestApiResponse;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -16,7 +18,7 @@ import org.springframework.web.bind.annotation.*;
 public class ReviewController {
     private final ReviewService reviewService;
     @PostMapping
-    public ResponseEntity<RestApiResponse> createReview(@RequestBody ReviewCreateRequestDto request){
+    public ResponseEntity<RestApiResponse> createReview(@RequestBody @Validated ReviewCreateRequestDto request){
         reviewService.registerReview(request);
         RestApiResponse response = new RestApiResponse(SuccessCode.REGISTER_SUCCESS);
         return ResponseEntity.ok(response);

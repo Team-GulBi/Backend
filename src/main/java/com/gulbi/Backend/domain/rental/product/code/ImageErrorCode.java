730bcb4f797416959ff965e3aa2598640de553da
woosungking <abuga991008@gmail.com>
2024-11-26 22:11:44 +0900

    refactor<ImageErrorCode> 예외코드 추가

--- src/main/java/com/gulbi/Backend/domain/rental/product/code/ImageErrorCode.java
+++ src/main/java/com/gulbi/Backend/domain/rental/product/code/ImageErrorCode.java
@@ -11,7 +11,8 @@ public enum ImageErrorCode implements ResponseApiCode {
     IMAGE_DELETE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "E200", "이미지 삭제에 실패했습니다."),
     INVALID_IMAGE_ID(HttpStatus.BAD_REQUEST, "E201", "잘못된 이미지 ID가 제공되었습니다."),
     DATABASE_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "E002", "이미지 관련 데이터베이스 오류가 발생했습니다."),
-    NOT_VALIDATED_IMAGE_URL(HttpStatus.BAD_REQUEST, "E002", "올바르지 않은 이미지 URL 형식 입니다.");
+    NOT_VALIDATED_IMAGE_URL(HttpStatus.BAD_REQUEST, "E002", "올바르지 않은 이미지 URL 형식 입니다."),
+    IMAGE_COLLECTION_IS_EMPTY(HttpStatus.BAD_REQUEST, "E002", "ImageCollection 안에 이미지가 존재하지 않습니다.");
 
 
 
@@ -28,16 +29,16 @@ public enum ImageErrorCode implements ResponseApiCode {
 
     @Override
     public HttpStatus getStatus() {
-        return null;
+        return this.status;
     }
 
     @Override
     public String getCode() {
-        return "";
+        return this.code;
     }
 
     @Override
     public String getMessage() {
-        return "";
+        return this.message;
     }
 }

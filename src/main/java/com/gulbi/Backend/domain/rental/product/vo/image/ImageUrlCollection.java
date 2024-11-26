8384f346dab9d2a14f92bf64d86b5417c793023d
woosungking <abuga991008@gmail.com>
2024-11-26 18:33:11 +0900

    feat<> 유효성 검사 추가

--- src/main/java/com/gulbi/Backend/domain/rental/product/vo/image/ImageUrlCollection.java
+++ src/main/java/com/gulbi/Backend/domain/rental/product/vo/image/ImageUrlCollection.java
@@ -1,7 +1,11 @@
 package com.gulbi.Backend.domain.rental.product.vo.image;
 
+import com.gulbi.Backend.domain.rental.product.code.ProductErrorCode;
+import com.gulbi.Backend.domain.rental.product.exception.ImageUrlCollectionException;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class ImageUrlCollection {
     private final List<ImageUrl> imageUrlList;
@@ -10,12 +14,19 @@ public class ImageUrlCollection {
         this.imageUrlList = imageUrlList;
     }
     public static ImageUrlCollection of(List<ImageUrl> imageUrlList){
+        if (imageUrlList.isEmpty()){
+            throw new ImageUrlCollectionException.ImageUrlNotFoundException(ProductErrorCode.IMAGEURL_NOT_FOUND);
+        }
         return new ImageUrlCollection(imageUrlList);
     }
     public List<ImageUrl> getImageUrls(){
         return new ArrayList<>(imageUrlList);
     }
-    public ImageUrl getMainImageUrl(){
-        return getImageUrls().get(0);
+    public ImageUrl getMainImageUrl() {
+        return Optional.ofNullable(getImageUrls().isEmpty() ? null : getImageUrls().get(0))
+                .orElseThrow(() -> new ImageUrlCollectionException.ImageUrlNotFoundException(ProductErrorCode.IMAGEURL_NOT_FOUND));
     }
+
+
+
 }

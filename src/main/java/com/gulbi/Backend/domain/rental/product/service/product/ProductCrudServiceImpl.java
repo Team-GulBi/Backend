8eb18e4365efe45f830a1a3eb98f8ae9bb5f5b3c
woosungking <abuga991008@gmail.com>
2024-11-26 17:28:00 +0900

    refactor<ProductCrudServiceImpl> 예외처리

--- src/main/java/com/gulbi/Backend/domain/rental/product/service/product/ProductCrudServiceImpl.java
+++ src/main/java/com/gulbi/Backend/domain/rental/product/service/product/ProductCrudServiceImpl.java
@@ -1,10 +1,13 @@
 package com.gulbi.Backend.domain.rental.product.service.product;
 
+import com.gulbi.Backend.domain.rental.product.code.ProductErrorCode;
+import com.gulbi.Backend.domain.rental.product.code.ProductSuccessCode;
 import com.gulbi.Backend.domain.rental.product.dto.product.ProductDto;
 import com.gulbi.Backend.domain.rental.product.dto.product.ProductOverViewResponse;
 import com.gulbi.Backend.domain.rental.product.dto.product.request.ProductUpdateRequestDto;
 import com.gulbi.Backend.domain.rental.product.entity.Category;
 import com.gulbi.Backend.domain.rental.product.entity.Product;
+import com.gulbi.Backend.domain.rental.product.exception.ProductException;
 import com.gulbi.Backend.domain.rental.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -25,32 +28,44 @@ public class ProductCrudServiceImpl implements ProductCrudService{
     @Override
     public ProductDto getProductDtoById(Long productId) {
         return productRepository.findProductDtoById(productId)
-                .orElseThrow(() -> new RuntimeException("Product not found: " + productId));
+                .orElseThrow(
+                        () -> new ProductException.ProductNotFoundException(ProductErrorCode.PRODUCT_NOT_FOUND));
     }
 
     @Override
     public Product getProductById(Long productId) {
-        return productRepository.findProductById(productId).orElseThrow();
+        return productRepository.findProductById(productId)
+                .orElseThrow(
+                     () -> new ProductException.ProductNotFoundException(ProductErrorCode.PRODUCT_NOT_FOUND)
+        );
     }
 
     @Override
     public List<ProductOverViewResponse> getProductOverViewByTitle(String title) {
-        return productRepository.findProductsByTitle(title);
+        return Optional.ofNullable(productRepository.findProductsByTitle(title))
+                .orElseThrow(
+                        ()-> new ProductException.NoProductFoundForTitleException(ProductErrorCode.PRODUCT_NOT_FOUND_BY_TITLE));
     }
 
     @Override
     public List<ProductOverViewResponse> getProductOverViewByTag(String tag, String tag2, String tag3) {
-        return productRepository.findProductsByTag(tag, tag2, tag3);
+        return Optional.ofNullable(productRepository.findProductsByTag(tag,tag2,tag3))
+                .orElseThrow(
+                        ()-> new ProductException.NoProductFoundForTitleException(ProductErrorCode.PRODUCT_NOT_FOUND_BY_TAGS));
     }
 
     @Override
     public void updateProductViews(Long productId){
-        productRepository.updateProductViews(productId);
+        Optional.ofNullable(productRepository.updateProductViews(productId))
+                .filter(updatedRows -> updatedRows > 0)
+                .orElseThrow(
+                        () -> new ProductException.NoUpdateProductException(ProductErrorCode.NO_UPDATED_COLUMNS)
+                );
     }
 
     @Override
     public void updateProductInfo(ProductUpdateRequestDto productUpdateRequestDto) {
-        Optional.ofNullable(productUpdateRequestDto.getCategoryInProduct())
+            Optional.ofNullable(productUpdateRequestDto.getCategoryInProduct())
                 .ifPresentOrElse(
                         dto -> productRepository.updateProductInfo(
                                 productUpdateRequestDto,

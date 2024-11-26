8c80114fabe9a800af2eaef29429c2cf13938243
Taeho KIM <laferrari963@naver.com>
2024-11-26 13:58:33 +0900

    rabbitconfig Jackson2Json 라이브러리 사용

--- src/main/java/com/gulbi/Backend/global/config/RabbitConfig.java
+++ src/main/java/com/gulbi/Backend/global/config/RabbitConfig.java
@@ -5,19 +5,22 @@ import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.messaging.converter.MessageConverter;
 
 @Configuration
 public class RabbitConfig {
+    private static final String QUEUE_NAME = "chat-queue";
 
     @Bean
     public Queue chatQueue() {
-        return new Queue("chat-queue", true); // durable = true
+        return new Queue(QUEUE_NAME, true);
     }
 
     @Bean
     public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
         RabbitTemplate template = new RabbitTemplate(connectionFactory);
-        template.setMessageConverter(new org.springframework.amqp.support.converter.SimpleMessageConverter());
+        template.setMessageConverter(new Jackson2JsonMessageConverter());
         return template;
     }
 }

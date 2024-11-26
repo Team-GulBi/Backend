a9eac3967d4ad31542ab00132155d1bf044846bf
woosungking <abuga991008@gmail.com>
2024-11-26 19:48:41 +0900

    feat<ImageUrl> 예외처리

--- src/main/java/com/gulbi/Backend/domain/rental/product/vo/image/ImageUrl.java
+++ src/main/java/com/gulbi/Backend/domain/rental/product/vo/image/ImageUrl.java
@@ -1,17 +1,30 @@
 package com.gulbi.Backend.domain.rental.product.vo.image;
 
+import com.gulbi.Backend.domain.rental.product.code.ImageErrorCode;
+import com.gulbi.Backend.domain.rental.product.exception.ImageVoException;
 import lombok.Getter;
+import org.apache.xmlbeans.impl.regex.Match;
+
+import java.util.regex.Pattern;
 
 public class ImageUrl {
+    private static final String REGEX = "^https://.*";
     @Getter
     private final String imageUrl;
 
     private ImageUrl(String imageUrl) {
+        ValidateImageUrl(imageUrl);
         this.imageUrl = imageUrl;
     }
 
     public static ImageUrl of(String imageUrl){
         return new ImageUrl(imageUrl);
+    }
+
+    private void ValidateImageUrl(String imageUrl){
+        if (!Pattern.matches(REGEX, imageUrl)){
+            throw new ImageVoException.NotValidatedImageUrlException(ImageErrorCode.NOT_VALIDATED_IMAGE_URL);
+        }
     }
 
 

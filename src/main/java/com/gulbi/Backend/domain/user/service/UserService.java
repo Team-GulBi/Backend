d25c1e5175d828a88e6bedd4ed5086b25f9ac22a
Taeho KIM <laferrari963@naver.com>
2024-11-30 17:50:31 +0900

    feat(UserService): id기반 User객체 반환

--- src/main/java/com/gulbi/Backend/domain/user/service/UserService.java
+++ src/main/java/com/gulbi/Backend/domain/user/service/UserService.java
@@ -59,4 +59,9 @@ public class UserService {
         return userRepository.findByEmail(email)
                 .orElseThrow(() -> new RuntimeException("User not found with email: " + email));
     }
+    public User getUserById(Long id) {
+        return userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found with id: " + id));
+    }
+
 }

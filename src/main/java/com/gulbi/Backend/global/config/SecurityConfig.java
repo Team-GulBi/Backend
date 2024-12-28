bd5146248ace936dbe8adb9be766cc631d4d8a78
Jinwoo Kim <99790907+jinu0328@users.noreply.github.com>
2024-12-28 18:49:01 +0900

    Update SecurityConfig.java

--- src/main/java/com/gulbi/Backend/global/config/SecurityConfig.java
+++ src/main/java/com/gulbi/Backend/global/config/SecurityConfig.java
@@ -45,11 +45,7 @@ public class SecurityConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOriginPatterns(
-                        "https://yajoba-frontend.vercel.app",
-                        "http://localhost:*",
-                        "http://192.168.50.*:5173"
-                )
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true); // 쿠키 허용

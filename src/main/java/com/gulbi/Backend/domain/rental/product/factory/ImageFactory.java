773f686ee35af900d59b1c223814b48adce4e1de
woosungking <abuga991008@gmail.com>
2024-12-29 17:10:52 +0900

    refactor<> 이미지 팩토리 변수명 변경 및 메인이미지 일반이미지 구분로직 추가

--- src/main/java/com/gulbi/Backend/domain/rental/product/factory/ImageFactory.java
+++ src/main/java/com/gulbi/Backend/domain/rental/product/factory/ImageFactory.java
@@ -6,20 +6,25 @@ import com.gulbi.Backend.domain.rental.product.vo.image.ImageUrl;
 import com.gulbi.Backend.domain.rental.product.vo.image.ImageUrlCollection;
 import com.gulbi.Backend.domain.rental.product.vo.image.ImageCollection;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class ImageFactory {
-    public static Image createImage(String url, Product product){
-        return Image.builder().url(url).product(product).build();
+    private static final boolean FALSE_MAIN_IMG = Boolean.FALSE;
+    private static final boolean TRUE_MAIN_IMG = Boolean.TRUE;
+
+    public static Image createImageToMain(ImageUrl url, Product product){
+        return Image.builder().url(url.getImageUrl()).product(product).main(TRUE_MAIN_IMG).build();
     }
     public static Image createImage(ImageUrl url, Product product){
-        return Image.builder().url(url.getImageUrl()).product(product).build();
+        return Image.builder().url(url.getImageUrl()).product(product).main(FALSE_MAIN_IMG).build();
     }
-    public static ImageCollection createImagesFromUrls(ImageUrlCollection imageUrlCollection, Product product) {
-        List<ImageUrl> urls = imageUrlCollection.getImageUrls();
+    public static ImageCollection createImages(ImageUrlCollection imageUrlCollection, Product product) {
+        List<ImageUrl> urls = imageUrlCollection != null ? imageUrlCollection.getImageUrls() : Collections.emptyList();
         return ImageCollection.of(urls.stream()
                 .map(url -> createImage(url, product))
                 .collect(Collectors.toList()));
     }
+
 }

f842a5508394bb6dde58e7d3d46422088233b35a
Taeho KIM <laferrari963@naver.com>
2025-01-02 19:20:41 +0900

    refactor(AuthController):엔드포인트 수정

--- src/main/java/com/gulbi/Backend/domain/user/controller/AuthController.java
+++ src/main/java/com/gulbi/Backend/domain/user/controller/AuthController.java
@@ -12,7 +12,7 @@ import java.util.Collections;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/v1/auth")
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class AuthController {
 

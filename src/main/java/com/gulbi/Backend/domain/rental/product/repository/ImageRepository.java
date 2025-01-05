42b144c594fa4e9fe142fedaea60c6be7485b986
woosungking <abuga991008@gmail.com>
2025-01-05 20:14:36 +0900

    feat<> 이미지 필드 중 메인표시 필드 토글 구현

--- src/main/java/com/gulbi/Backend/domain/rental/product/repository/ImageRepository.java
+++ src/main/java/com/gulbi/Backend/domain/rental/product/repository/ImageRepository.java
@@ -3,6 +3,7 @@ package com.gulbi.Backend.domain.rental.product.repository;
 import com.gulbi.Backend.domain.rental.product.dto.ProductImageDto;
 import com.gulbi.Backend.domain.rental.product.dto.product.request.ProductImageDeleteRequestDto;
 import com.gulbi.Backend.domain.rental.product.entity.Image;
+import com.gulbi.Backend.domain.rental.product.entity.Product;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -22,6 +23,16 @@ public interface ImageRepository extends JpaRepository<Image,Long > {
     @Modifying
     @Query("DELETE FROM Image i WHERE i.id IN :#{#dto.imagesId}")
     void deleteImages(@Param("dto") ProductImageDeleteRequestDto dto);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Image i SET i.main = false WHERE i.product = :product")
+    void resetMainImagesByProduct(@Param("product") Product product);
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Image i SET i.main = true WHERE i.url = :imageUrl")
+    void updateImagesFlagsToTrue(@Param("imageUrl") String imageUrl);
 
 
 }

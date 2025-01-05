aa1220c5d0c161d962e6425bd197708eadb73875
woosungking <abuga991008@gmail.com>
2025-01-05 20:17:49 +0900

    refactor<ProductServiceImpl> 이미지관련, 상품텍스트 관련 dto분리

--- src/main/java/com/gulbi/Backend/domain/rental/product/service/product/ProductServiceImpl.java
+++ src/main/java/com/gulbi/Backend/domain/rental/product/service/product/ProductServiceImpl.java
@@ -5,10 +5,9 @@ import com.gulbi.Backend.domain.rental.product.dto.product.request.*;
 import com.gulbi.Backend.domain.rental.product.dto.product.request.register.ProductImageCreateRequestDto;
 import com.gulbi.Backend.domain.rental.product.dto.product.request.register.ProductMainImageCreateRequestDto;
 import com.gulbi.Backend.domain.rental.product.dto.product.request.register.ProductRegisterRequestDto;
-import com.gulbi.Backend.domain.rental.product.dto.product.request.update.ProductCategoryUpdateRequestDto;
-import com.gulbi.Backend.domain.rental.product.dto.product.request.update.ProductExistingMainImageUpdateRequestDto;
-import com.gulbi.Backend.domain.rental.product.dto.product.request.update.ProductUpdateRequestDto;
 import com.gulbi.Backend.domain.rental.product.dto.product.response.ProductDetailResponseDto;
+import com.gulbi.Backend.domain.rental.product.dto.product.update.ProductImageInfoUpdateDto;
+import com.gulbi.Backend.domain.rental.product.dto.product.update.ProductInfoUpdateDto;
 import com.gulbi.Backend.domain.rental.product.service.product.register.ProductRegistrationService;
 import com.gulbi.Backend.domain.rental.product.service.product.search.ProductSearchService;
 import com.gulbi.Backend.domain.rental.product.service.product.update.ProductUpdatingService;
@@ -44,15 +43,8 @@ public class ProductServiceImpl implements ProductService {
     }
 
     @Override
-    public void updateProduct(ProductUpdateRequestDto toBeUpdatedProductInfo,
-                              ProductCategoryUpdateRequestDto toBeUpdatedCategories,
-                              ProductImageCreateRequestDto toBeAddedImages,
-                              ProductImageCreateRequestDto toBeUpdatedMainImage,
-                              ProductExistingMainImageUpdateRequestDto toBeUpdateMainImageWithUrl,
-                              ProductImageDeleteRequestDto toBeDeletedImages
-                              ){
-        productUpdatingService.updateProductInfo(toBeUpdatedProductInfo, toBeUpdatedCategories,toBeAddedImages, toBeUpdatedMainImage, toBeUpdateMainImageWithUrl, toBeDeletedImages);
-        //
+    public void updateProduct(ProductInfoUpdateDto productInfoUpdateDto, ProductImageInfoUpdateDto productImageInfoUpdateDto){
+        productUpdatingService.updateProductInfo(productInfoUpdateDto, productImageInfoUpdateDto);
     }
 
 }

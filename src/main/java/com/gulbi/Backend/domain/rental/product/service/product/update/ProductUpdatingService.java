619e88d6fbb989b8aa9aae482ac17ad411044448
woosungking <abuga991008@gmail.com>
2025-01-05 20:15:46 +0900

    refactor<> 이미지 관련, 상품관련 dto분리

--- src/main/java/com/gulbi/Backend/domain/rental/product/service/product/update/ProductUpdatingService.java
+++ src/main/java/com/gulbi/Backend/domain/rental/product/service/product/update/ProductUpdatingService.java
@@ -1,17 +1,9 @@
 package com.gulbi.Backend.domain.rental.product.service.product.update;
 
-import com.gulbi.Backend.domain.rental.product.dto.product.request.update.ProductCategoryUpdateRequestDto;
-import com.gulbi.Backend.domain.rental.product.dto.product.request.register.ProductImageCreateRequestDto;
-import com.gulbi.Backend.domain.rental.product.dto.product.request.ProductImageDeleteRequestDto;
-import com.gulbi.Backend.domain.rental.product.dto.product.request.update.ProductExistingMainImageUpdateRequestDto;
-import com.gulbi.Backend.domain.rental.product.dto.product.request.update.ProductUpdateRequestDto;
+import com.gulbi.Backend.domain.rental.product.dto.product.update.ProductImageInfoUpdateDto;
+import com.gulbi.Backend.domain.rental.product.dto.product.update.ProductInfoUpdateDto;
 
 public interface ProductUpdatingService {
     public void updateProductViews(Long productId);
-    public void updateProductInfo(ProductUpdateRequestDto toBeUpdatedProductInfo,
-                                  ProductCategoryUpdateRequestDto toBeUpdatedCategories,
-                                  ProductImageCreateRequestDto toBeAddedImages,
-                                  ProductImageCreateRequestDto toBeUpdatedMainImage,
-                                  ProductExistingMainImageUpdateRequestDto toBeUpdateMainImageWithUrl,
-                                  ProductImageDeleteRequestDto toBeDeletedImages);
+    public void updateProductInfo(ProductInfoUpdateDto productInfoUpdateDto, ProductImageInfoUpdateDto productImageInfoUpdateDto);
 }

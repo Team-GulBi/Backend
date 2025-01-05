c8a55907904b93d851704b461c51f4cf54b05fb1
woosungking <abuga991008@gmail.com>
2025-01-05 20:16:03 +0900

    refactor<> 조건문 추가 후 null 처리

--- src/main/java/com/gulbi/Backend/domain/rental/product/service/product/update/ProductUpdateServiceImpl.java
+++ src/main/java/com/gulbi/Backend/domain/rental/product/service/product/update/ProductUpdateServiceImpl.java
@@ -1,15 +1,14 @@
 package com.gulbi.Backend.domain.rental.product.service.product.update;
 
+import com.gulbi.Backend.domain.rental.product.dto.category.CategoryInProductDto;
 import com.gulbi.Backend.domain.rental.product.dto.product.request.update.ProductCategoryUpdateRequestDto;
-import com.gulbi.Backend.domain.rental.product.dto.product.request.register.ProductImageCreateRequestDto;
-import com.gulbi.Backend.domain.rental.product.dto.product.request.ProductImageDeleteRequestDto;
-import com.gulbi.Backend.domain.rental.product.dto.product.request.update.ProductExistingMainImageUpdateRequestDto;
 import com.gulbi.Backend.domain.rental.product.dto.product.request.update.ProductUpdateRequestDto;
+import com.gulbi.Backend.domain.rental.product.dto.product.update.ProductImageInfoUpdateDto;
+import com.gulbi.Backend.domain.rental.product.dto.product.update.ProductInfoUpdateDto;
 import com.gulbi.Backend.domain.rental.product.entity.Product;
 import com.gulbi.Backend.domain.rental.product.service.category.CategoryBusinessService;
-import com.gulbi.Backend.domain.rental.product.service.image.ImageCrudService;
+import com.gulbi.Backend.domain.rental.product.service.image.ImageService;
 import com.gulbi.Backend.domain.rental.product.service.product.crud.ProductCrudService;
-import com.gulbi.Backend.domain.rental.product.vo.image.ImageUrlCollection;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -25,7 +24,7 @@ import java.util.Optional;
 public class ProductUpdateServiceImpl implements ProductUpdatingService{
     private final ProductCrudService productCrudService;
     private final CategoryBusinessService categoryBusinessService;
-    private final ImageCrudService imageCrudService;
+    private final ImageService imageService;
     @Override
     public void updateProductViews(Long productId){
         resolveProduct(productId); // 유효성 검사
@@ -33,30 +32,43 @@ public class ProductUpdateServiceImpl implements ProductUpdatingService{
     }
 
     //ToDo: 람다식들 순서 배열 중요함, 추후 분리 할때 해당 메서드를 퍼사드로 하고, 나머지는 분리
+    //
     @Override
-    public void updateProductInfo(
-            ProductUpdateRequestDto toBeUpdatedProductInfo,
-            ProductCategoryUpdateRequestDto toBeUpdatedCategories,
-            ProductImageCreateRequestDto toBeAddedImages,
-            ProductImageCreateRequestDto toBeUpdatedMainImage,
-            ProductExistingMainImageUpdateRequestDto toBeUpdatedMainImageWithUrl,
-            ProductImageDeleteRequestDto toBeDeletedImages
-    ){
-
-        resolveProduct(toBeUpdatedProductInfo.getProductId());
-        // 카테고리를 만약 업데이트를 한다면 카테고리 아이디를 기반으로 객체를 만들어서 상품dto에 넣어줘야함.
-        Optional.ofNullable(toBeUpdatedCategories)
-                .map(categoryBusinessService::resolveCategories)
-                .ifPresent(toBeUpdatedProductInfo::setCategoryInProduct);
+    public void updateProductInfo(ProductInfoUpdateDto productInfoUpdateDto, ProductImageInfoUpdateDto productImageInfoUpdateDto){
+        if(productImageInfoUpdateDto !=null){
+            updateProductTextInfo(productInfoUpdateDto);
+        }
+        if(productImageInfoUpdateDto !=null) {
+            imageService.updateProductImages(productImageInfoUpdateDto);
+        }
+    }
 
 
-        //상품 업데이트
-        Optional.of(toBeUpdatedProductInfo)
-                .ifPresent(productCrudService::updateProductInfo);
+    private void updateProductTextInfo(ProductInfoUpdateDto productInfoUpdateDto){
+        ProductUpdateRequestDto productUpdateRequestDto = ProductUpdateRequestDto.of();
+        CategoryInProductDto categoryInProductDto = null;
+        ProductCategoryUpdateRequestDto productCategoryUpdateRequestDto = null;
+
+        if(productInfoUpdateDto.getProductUpdateRequestDto()!=null){
+            productUpdateRequestDto = productInfoUpdateDto.getProductUpdateRequestDto();
+        }
+
+        if(productInfoUpdateDto.getProductCategoryUpdateRequestDto() !=null) {
+            productCategoryUpdateRequestDto = productInfoUpdateDto.getProductCategoryUpdateRequestDto();
+            categoryInProductDto = categoryBusinessService.resolveCategories(productCategoryUpdateRequestDto);
+        }
+
+        productUpdateRequestDto.setCategoryInProduct(categoryInProductDto);
+
+        Long productId = productInfoUpdateDto.getProductId();
+        productUpdateRequestDto.setProductId(productId);
 
 
+
+        productCrudService.updateProductInfo(productUpdateRequestDto);
 
     }
+
 
     private Product resolveProduct(Long productId){
         return productCrudService.getProductById(productId);

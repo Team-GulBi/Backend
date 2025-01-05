525ed1779eb07f4cfa4b0311caaf382d733e329d
woosungking <abuga991008@gmail.com>
2025-01-05 20:17:54 +0900

    refactor<ProductServiceImpl> 이미지관련, 상품텍스트 관련 dto분리

--- src/main/java/com/gulbi/Backend/domain/rental/product/service/product/ProductService.java
+++ src/main/java/com/gulbi/Backend/domain/rental/product/service/product/ProductService.java
@@ -5,10 +5,9 @@ import com.gulbi.Backend.domain.rental.product.dto.product.request.*;
 import com.gulbi.Backend.domain.rental.product.dto.product.request.register.ProductImageCreateRequestDto;
 import com.gulbi.Backend.domain.rental.product.dto.product.request.register.ProductMainImageCreateRequestDto;
 import com.gulbi.Backend.domain.rental.product.dto.product.request.register.ProductRegisterRequestDto;
-import com.gulbi.Backend.domain.rental.product.dto.product.request.update.ProductCategoryUpdateRequestDto;
-import com.gulbi.Backend.domain.rental.product.dto.product.request.update.ProductExistingMainImageUpdateRequestDto;
-import com.gulbi.Backend.domain.rental.product.dto.product.request.update.ProductUpdateRequestDto;
 import com.gulbi.Backend.domain.rental.product.dto.product.response.ProductDetailResponseDto;
+import com.gulbi.Backend.domain.rental.product.dto.product.update.ProductImageInfoUpdateDto;
+import com.gulbi.Backend.domain.rental.product.dto.product.update.ProductInfoUpdateDto;
 
 import java.util.List;
 
@@ -17,10 +16,5 @@ public interface ProductService {
     public void registrationProduct(ProductRegisterRequestDto productRegisterRequestDto, ProductImageCreateRequestDto productImageCreateRequestDto, ProductMainImageCreateRequestDto productMainImageCreateRequestDto);
     public ProductDetailResponseDto getProductDetail(Long productId);
     public void updateProductViews(Long productId);
-    public void updateProduct(ProductUpdateRequestDto toBeUpdatedProductInfo,
-                              ProductCategoryUpdateRequestDto toBeUpdatedCategories,
-                              ProductImageCreateRequestDto toBeAddedImages,
-                              ProductImageCreateRequestDto toBeUpdatedMainImage,
-                              ProductExistingMainImageUpdateRequestDto toBeUpdateMainImageWithUrl,
-                              ProductImageDeleteRequestDto toBeDeletedImages);
+    public void updateProduct(ProductInfoUpdateDto productInfoUpdateDto, ProductImageInfoUpdateDto productImageInfoUpdateDto);
 }

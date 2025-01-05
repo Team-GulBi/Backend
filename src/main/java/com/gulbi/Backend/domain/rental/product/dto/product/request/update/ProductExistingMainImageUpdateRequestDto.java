1da67f88ec70222bf57677551dd638c1768b0e9e
woosungking <abuga991008@gmail.com>
2025-01-05 20:21:25 +0900

    feat<ProductExistingMainImageUpdateRequestDto>

--- src/main/java/com/gulbi/Backend/domain/rental/product/dto/product/request/update/ProductExistingMainImageUpdateRequestDto.java
+++ src/main/java/com/gulbi/Backend/domain/rental/product/dto/product/request/update/ProductExistingMainImageUpdateRequestDto.java
@@ -1,9 +1,20 @@
 package com.gulbi.Backend.domain.rental.product.dto.product.request.update;
 
+import com.gulbi.Backend.domain.rental.product.vo.image.ImageUrl;
 import lombok.Getter;
 
 @Getter
 public class ProductExistingMainImageUpdateRequestDto {
-    private String mainImageUrl;
+    private ImageUrl mainImageUrl;
 
+
+    private ProductExistingMainImageUpdateRequestDto() {}
+
+    private ProductExistingMainImageUpdateRequestDto(String mainImageUrl) {
+        this.mainImageUrl = ImageUrl.of(mainImageUrl);
+    }
+
+    public static ProductExistingMainImageUpdateRequestDto of(String mainImageUrl) {
+        return new ProductExistingMainImageUpdateRequestDto(mainImageUrl);
+    }
 }

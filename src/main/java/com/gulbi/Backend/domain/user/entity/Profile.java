738c15bd4445907f52ed20349c4c422e710dd015
Taeho KIM <laferrari963@naver.com>
2025-01-05 21:39:57 +0900

    불필요 import 삭제

--- src/main/java/com/gulbi/Backend/domain/user/entity/Profile.java
+++ src/main/java/com/gulbi/Backend/domain/user/entity/Profile.java
@@ -6,9 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
 import jakarta.persistence.*;
-import java.time.LocalDateTime;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 

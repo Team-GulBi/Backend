08f66eacc94b0413b52a26b804bf34991c935ff0
woosungking <abuga991008@gmail.com>
2025-01-10 23:08:52 +0900

    feat<.deleteProduct>

--- src/main/java/com/gulbi/Backend/domain/rental/product/service/product/ProductServiceImpl.java
+++ src/main/java/com/gulbi/Backend/domain/rental/product/service/product/ProductServiceImpl.java
@@ -8,6 +8,7 @@ import com.gulbi.Backend.domain.rental.product.dto.product.request.register.Prod
 import com.gulbi.Backend.domain.rental.product.dto.product.response.ProductDetailResponseDto;
 import com.gulbi.Backend.domain.rental.product.dto.product.update.ProductImageInfoUpdateDto;
 import com.gulbi.Backend.domain.rental.product.dto.product.update.ProductInfoUpdateDto;
+import com.gulbi.Backend.domain.rental.product.service.product.delete.ProductDeleteService;
 import com.gulbi.Backend.domain.rental.product.service.product.register.ProductRegistrationService;
 import com.gulbi.Backend.domain.rental.product.service.product.search.ProductSearchService;
 import com.gulbi.Backend.domain.rental.product.service.product.update.ProductUpdatingService;
@@ -23,6 +24,7 @@ public class ProductServiceImpl implements ProductService {
     private final ProductRegistrationService productRegistrationService;
     private final ProductSearchService productSearchService;
     private final ProductUpdatingService productUpdatingService;
+    private final ProductDeleteService productDeleteService;
 
     @Override
     public List<ProductOverViewResponse> searchProductOverview(ProductSearchRequestDto productSearchRequestDto) {
@@ -45,6 +47,11 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public void updateProduct(ProductInfoUpdateDto productInfoUpdateDto, ProductImageInfoUpdateDto productImageInfoUpdateDto){
         productUpdatingService.updateProductInfo(productInfoUpdateDto, productImageInfoUpdateDto);
+    }
+
+    @Override
+    public void deleteProduct(Long productId) {
+        productDeleteService.deleteProductInfo(productId);
     }
 
 }

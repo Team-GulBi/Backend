be2d1e00745d528c1b7c9cc5303fb4fe84fe5501
woosungking <abuga991008@gmail.com>
2025-01-27 23:59:41 +0900

    refactor<> 스택트레이스가 없는 로그의경우 warn 레벨로 처리

--- src/main/java/com/gulbi/Backend/global/error/GlobalExceptionHandler.java
+++ src/main/java/com/gulbi/Backend/global/error/GlobalExceptionHandler.java
@@ -10,16 +10,29 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Objects;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     private final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
     @ExceptionHandler(BusinessException.class)
     public ResponseEntity<RestApiResponse> handleBusinessException(BusinessException e){
-        RestApiResponse response = new RestApiResponse(e.getResponseApiCode());
-        MDC.put("exceptionCauseArgs", e.getMetaDataDto().getArgs());
-        MDC.put("exceptionClass", e.getMetaDataDto().getClassName());
-        logger.error(e.getMessage());
-        return ResponseEntity.status(e.getStatus()).body(response);
+        RestApiResponse response = new RestApiResponse(e.getMetaData().getResponseApiCode());
+        if (!Objects.equals(e.getMetaData().getStackTrace(), "") &&
+                !Objects.equals(e.getMetaData().getStackTrace(), "NO_STACKTRACE_IN_THIS_EXCEPTION")) {
+            MDC.put("exceptionCauseArgs", e.getMetaData().getArgs());
+            MDC.put("exceptionClass", e.getMetaData().getClassName());
+            MDC.put("stackTrace",e.getMetaData().getStackTrace());
+            logger.error(e.getMessage());
+            return ResponseEntity.status(e.getMetaData().getResponseApiCode().getStatus()).body(response);
+        }
+
+        MDC.put("exceptionClass", e.getMetaData().getClassName());
+        MDC.put("stackTrace",e.getMetaData().getStackTrace());
+        logger.warn(e.getMessage());
+        return ResponseEntity.status(e.getMetaData().getResponseApiCode().getStatus()).body(response);
+
     }
 
 

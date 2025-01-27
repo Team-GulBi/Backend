f4be227e53fc5ec8eda0e7b515344b7567af8d89
woosungking <abuga991008@gmail.com>
2025-01-27 23:58:38 +0900

    refactor<> metaData 필드로 축소

--- src/main/java/com/gulbi/Backend/global/error/BusinessException.java
+++ src/main/java/com/gulbi/Backend/global/error/BusinessException.java
@@ -5,33 +5,15 @@ import lombok.Getter;
 import org.springframework.http.HttpStatus;
 @Getter
 public class BusinessException extends RuntimeException{
-    private HttpStatus status;
-    private String code;
-    private ResponseApiCode responseApiCode;
-    private ExceptionMetaData metaDataDto;
+    private ExceptionMetaData metaData;
 
-    public BusinessException(ResponseApiCode errorCode, ExceptionMetaData metaDataDto){
-        super(errorCode.getMessage());
-        this.metaDataDto = metaDataDto;
-        this.responseApiCode = errorCode;
-        this.status=errorCode.getStatus();
-        this.code=errorCode.getCode();
+    public BusinessException(ExceptionMetaData metaDataDto){
+        super(metaDataDto.getResponseApiCode().getMessage());
+        this.metaData = metaDataDto;
     }
 
-    public HttpStatus getStatus() {
-        return status;
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public ResponseApiCode getResponseApiCode() {
-        return responseApiCode;
-    }
-
-    public ExceptionMetaData getMetaDataDto() {
-        return metaDataDto;
+    public ExceptionMetaData getStatus() {
+        return metaData;
     }
 }
 

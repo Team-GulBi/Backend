d47439185105ed79941b622688a1f268670e4030
woosungking <abuga991008@gmail.com>
2025-02-19 18:19:08 +0900

    refactor<> 스웨거 문서 수정

--- src/main/java/com/gulbi/Backend/domain/rental/product/dto/product/request/update/ProductExistingMainImageUpdateRequestDto.java
+++ src/main/java/com/gulbi/Backend/domain/rental/product/dto/product/request/update/ProductExistingMainImageUpdateRequestDto.java
@@ -1,10 +1,12 @@
 package com.gulbi.Backend.domain.rental.product.dto.product.request.update;
 
 import com.gulbi.Backend.domain.rental.product.vo.image.ImageUrl;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
 public class ProductExistingMainImageUpdateRequestDto {
+    @Schema(description = "메인 이미지 URL", example = "https://yajoba.s3.ap-northeast-2.amazonaws.com/images/b45dce5c-31f3-4ebf-a1eb-e6c77ae8a846_chill2.jpeg", type = "string")
     private ImageUrl mainImageUrl;
 
 

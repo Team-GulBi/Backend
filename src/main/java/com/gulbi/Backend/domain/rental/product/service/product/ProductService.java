4c7d937700f20cac99811c00e112c6c200294944
woosungking <abuga991008@gmail.com>
2025-02-19 16:13:47 +0900

    refactor<> 상품 등록 시 상품 아이디를 뱉도록 변경

--- src/main/java/com/gulbi/Backend/domain/rental/product/service/product/ProductService.java
+++ src/main/java/com/gulbi/Backend/domain/rental/product/service/product/ProductService.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 public interface ProductService {
     public List<ProductOverViewResponse> searchProductOverview(ProductSearchRequestDto productSearchRequestDto);
-    public void registrationProduct(ProductRegisterRequestDto productRegisterRequestDto, ProductImageCreateRequestDto productImageCreateRequestDto, ProductMainImageCreateRequestDto productMainImageCreateRequestDto);
+    public Long registrationProduct(ProductRegisterRequestDto productRegisterRequestDto, ProductImageCreateRequestDto productImageCreateRequestDto, ProductMainImageCreateRequestDto productMainImageCreateRequestDto);
     public ProductDetailResponseDto getProductDetail(Long productId);
     public void updateProductViews(Long productId);
     public void updateProduct(ProductInfoUpdateDto productInfoUpdateDto, ProductImageInfoUpdateDto productImageInfoUpdateDto);

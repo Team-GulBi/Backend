0edf12d09929c3e894ce82c4289f8e0dc49bfa69
woosungking <abuga991008@gmail.com>
2025-02-19 18:33:12 +0900

    refactor<> 반환값에 이미지 ID, Main여부 추가

--- src/main/java/com/gulbi/Backend/domain/rental/product/dto/ProductImageDto.java
+++ src/main/java/com/gulbi/Backend/domain/rental/product/dto/ProductImageDto.java
@@ -7,11 +7,13 @@ public class ProductImageDto {
     private final Long id;
     private final Long productId;
     private final String url;
+    private final boolean main;
 
     // Product 객체에서 필요한 필드를 가져오는 생성자
-    public ProductImageDto(Long id, Long productId, String url) {
+    public ProductImageDto(Long id, Long productId, String url, boolean main) {
         this.id = id;
         this.productId = productId;
         this.url = url;
+        this.main = main;
     }
 }

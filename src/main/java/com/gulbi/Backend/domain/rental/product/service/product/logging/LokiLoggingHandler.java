0f69f9734f4db5350f6f64187a3994928bef80e0
woosungking <abuga991008@gmail.com>
2025-02-23 16:10:16 +0900

    feat<> logHandler 작성
    로키,redis에 각각 전달을 위한 인터페이스 및 구현체 초안 작성

---
+++ src/main/java/com/gulbi/Backend/domain/rental/product/service/product/logging/LokiLoggingHandler.java
@@ -0,0 +1,8 @@
+package com.gulbi.Backend.domain.rental.product.service.product.logging;
+
+public class LokiLoggingHandler implements ProductLogHandler{
+    @Override
+    public void logData() {
+        
+    }
+}
---
+++ src/main/java/com/gulbi/Backend/domain/rental/product/service/product/logging/ProductLogHandler.java
@@ -0,0 +1,5 @@
+package com.gulbi.Backend.domain.rental.product.service.product.logging;
+
+public interface ProductLogHandler {
+    void logData();
+}

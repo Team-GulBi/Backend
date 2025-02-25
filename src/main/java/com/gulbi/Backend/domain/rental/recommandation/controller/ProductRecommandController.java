fc623a89e555d8ce444f7e217137b44aa9faef68
woosungking <abuga991008@gmail.com>
2025-02-25 15:22:10 +0900

    feat<ProductRecommandService> 추천 상품 컨트롤러 구현

--- src/main/java/com/gulbi/Backend/domain/rental/recommandation/controller/ProductRecommandController.java
+++ src/main/java/com/gulbi/Backend/domain/rental/recommandation/controller/ProductRecommandController.java
@@ -1,9 +1,17 @@
 package com.gulbi.Backend.domain.rental.recommandation.controller;
 
+import com.gulbi.Backend.domain.rental.product.code.ProductSuccessCode;
+import com.gulbi.Backend.domain.rental.product.dto.product.ProductOverViewResponse;
+import com.gulbi.Backend.domain.rental.recommandation.code.RecommandationSuccessCode;
 import com.gulbi.Backend.domain.rental.recommandation.service.ProductRecommandService;
+import com.gulbi.Backend.global.response.ResponseApiCode;
+import com.gulbi.Backend.global.response.RestApiResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/recommand")
@@ -15,7 +23,9 @@ public class ProductRecommandController {
     }
 
     @GetMapping
-    public void showRealTimePopularProduct(){
-        productRecommandService.getRealTimePopularProducts();
+    public ResponseEntity<RestApiResponse> showRealTimePopularProduct(){
+        List<ProductOverViewResponse> products = productRecommandService.getRealTimePopularProducts();
+        RestApiResponse response = new RestApiResponse(RecommandationSuccessCode.REALTIME_POPULAR_PRODUCTS_FOUND_SUCCESS,products);
+        return ResponseEntity.ok(response);
     }
 }

1a0be012a71dc151c1dee38745613c41224352d7
woosungking <abuga991008@gmail.com>
2025-02-25 15:21:21 +0900

    feat<.getProductOverViewByproductIds>

--- src/main/java/com/gulbi/Backend/domain/rental/product/service/product/crud/ProductCrudService.java
+++ src/main/java/com/gulbi/Backend/domain/rental/product/service/product/crud/ProductCrudService.java
@@ -14,6 +14,7 @@ public interface ProductCrudService {
     public Product getProductById(Long productId);
     public List<ProductOverViewResponse> getProductOverViewByTag(String tag, String tag2, String tag3);
     public List<ProductOverViewResponse> getProductOverViewByTitle(String title);
+    public List<ProductOverViewResponse> getProductOverViewByproductIds(List<Long> productIds);
     public void updateProductViews(Long productId);
     public void updateProductInfo(ProductUpdateRequestDto productUpdateRequestDto);
     public void updateProductMainImage(ProductMainImageUpdateDto productMainImageUpdateDto);

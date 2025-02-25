15b1b9ec860fb2f805a0ec6946fd3d2d348988f3
woosungking <abuga991008@gmail.com>
2025-02-25 19:58:39 +0900

    feat<ProductRecommandController> 카테고리 별 상품 조회 컨트롤러

--- src/main/java/com/gulbi/Backend/domain/rental/recommandation/controller/ProductRecommandController.java
+++ src/main/java/com/gulbi/Backend/domain/rental/recommandation/controller/ProductRecommandController.java
@@ -6,9 +6,11 @@ import com.gulbi.Backend.domain.rental.recommandation.code.RecommandationSuccess
 import com.gulbi.Backend.domain.rental.recommandation.service.ProductRecommandService;
 import com.gulbi.Backend.global.response.ResponseApiCode;
 import com.gulbi.Backend.global.response.RestApiResponse;
+import io.swagger.v3.oas.annotations.Parameter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -32,6 +34,16 @@ public class ProductRecommandController {
     @GetMapping("/recent")
     public ResponseEntity<RestApiResponse> showRecentRegistratedProducts(){
         List<ProductOverViewResponse> products = productRecommandService.getRecentRegistrationProducts();
+        RestApiResponse response = new RestApiResponse(RecommandationSuccessCode.REALTIME_POPULAR_PRODUCTS_FOUND_SUCCESS,products);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/category")
+    public ResponseEntity<RestApiResponse> showRecentProductByCategory(@Parameter(description = "대분류", required = true) @RequestParam("bCategoryId") Long bCategoryId,
+                                                                 @Parameter(description = "중분류", required = false) @RequestParam("mCategoryId") Long mCategoryId,
+                                                                 @Parameter(description = "소분류", required = false) @RequestParam("sCategoryId") Long sbCategoryId
+                                                                 ){
+        List<ProductOverViewResponse> products = productRecommandService.getRecentProductByCategory(bCategoryId, mCategoryId, sbCategoryId);
         RestApiResponse response = new RestApiResponse(RecommandationSuccessCode.REALTIME_POPULAR_PRODUCTS_FOUND_SUCCESS,products);
         return ResponseEntity.ok(response);
     }

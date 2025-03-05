99d538c51a276ac30a3ff3c7adcd9f5f7fc447c4
woosungking <abuga991008@gmail.com>
2025-03-05 21:36:20 +0900

    feat: 상품 추천 퍼사드 서비스

--- src/main/java/com/gulbi/Backend/domain/rental/recommandation/service/ProductRecommendFacade.java
+++ src/main/java/com/gulbi/Backend/domain/rental/recommandation/service/ProductRecommendFacade.java
@@ -1,15 +1,17 @@
 package com.gulbi.Backend.domain.rental.recommandation.service;
 
 import com.gulbi.Backend.domain.rental.product.dto.product.ProductOverViewResponse;
+import com.gulbi.Backend.domain.rental.recommandation.vo.PersonalRecommendationRequestDto;
+import com.gulbi.Backend.domain.rental.recommandation.vo.PersonalRecommendationResponseDto;
 import org.springframework.data.domain.Pageable;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-public interface ProductRecommandService {
+public interface ProductRecommendFacade {
     List<ProductOverViewResponse> getRealTimePopularProducts();
     List<ProductOverViewResponse> getRecentRegistrationProducts(Pageable pageable, LocalDateTime lastCreatedAt);
     List<ProductOverViewResponse> getRecentProductByCategory(Long bCategoryId, Long mCategoryId, Long sCategoryId, LocalDateTime lastCreatedAt, Pageable pageable);
-    List<ProductOverViewResponse> getPersonalizedRecommendationProducts();
+    PersonalRecommendationResponseDto getPersonalizedRecommendationProducts(PersonalRecommendationRequestDto personalRecommendationRequestDto);
 
 }

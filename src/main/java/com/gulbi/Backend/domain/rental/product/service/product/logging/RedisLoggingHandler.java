7d546f85ff7a436a3f9d3dcde387035df7219c96
Taeho KIM <laferrari963@naver.com>
2025-03-06 18:59:25 +0900

    main 병합

--- src/main/java/com/gulbi/Backend/domain/rental/product/service/product/logging/RedisLoggingHandler.java
+++
@@ -1,8 +0,0 @@
-package com.gulbi.Backend.domain.rental.product.service.product.logging;
-
-public class RedisLoggingHandler implements ProductLogHandler{
-    @Override
-    public void logData() {
-
-    }
-}

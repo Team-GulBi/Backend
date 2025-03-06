788c439d3f1d923db83e1727b5d36348f3423755
woosungking <abuga991008@gmail.com>
2025-03-06 14:35:47 +0900

    chore: merge

--- src/main/java/com/gulbi/Backend/domain/user/entity/User.java
+++ src/main/java/com/gulbi/Backend/domain/user/entity/User.java
@@ -36,8 +36,8 @@ public class User extends BaseEntity {
     @Column(nullable = false)
     private String password;
 
-    @OneToOne(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
-    private Profile profile;
+//    @OneToOne(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+//    private Profile profile;
 
     // 내가 만든 채팅방 (user1 역할)
     @OneToMany(mappedBy = "user1", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
